e895f5ecc1a9a02baddfea43e0159def567c4a82
patrick muchiri <muchiripatrick86@gmail.com>
2021-10-27 00:45:52 +0300

    rails blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,7 @@
+class BlogsController < ApplicationController
+  def index
+  end
+  # Append。render : new Is omitted.
+  def new
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  # Append
+  resources :blogs
+end
